f69b95a34c4b277d5b0814ab526e242355070cfe
DericM <dmccadden@my.bcit.ca>
2016-02-04 15:34:04 -0800

    init

--- src/Qfour.java
+++ src/Qfour.java
@@ -4,14 +4,12 @@ public class Qfour {
     public static void main (String args[]){
         int n = 50;
         int counter = 200;
-        System.out.println(counter);
-        
         for(int i = 2;i <= n+4;i++){
             counter = counter + 5;
-            for(int j = i+1;j < n+4;j++){
-                for(int k = j+1;k < n+4;k++){
+            for(int j = i+1;j <= n+4;j++){
+                for(int k = j+1;k <= n+4;k++){
                     counter = counter + 10;
-                    System.out.println(i + "|" + j + "|" + k);
+                    //System.out.println(i + "|" + j + "|" + k);
                 }
             }
         }
